62f1d1bd7427cd91c01e29e662e2722d5ef46edf
mahjabeenIshaq <mahjabeenishaq007@gmail.com>
2023-08-21 23:53:12 +0500

    tystQ1 complete

---
+++ ex1.js
@@ -0,0 +1,4 @@
+"use strict";
+let personName = "maha";
+let message = `"hello ${personName}", would you like to learn programming?"`;
+console.log(message);
---
+++ ex1.ts
@@ -0,0 +1,3 @@
+let personName:string = "maha";
+let message =`"hello ${personName}", would you like to learn programming?"`
+console.log(message)
